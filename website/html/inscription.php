75e0844e750f8c46a52262bf6b37564bb93f59ba
buriotVincent <buriot.vincent@gmail.com>
2020-02-04 16:48:50 +0100

    inputs

--- website/html/inscription.php
+++ website/html/inscription.php
@@ -25,7 +25,7 @@ include 'config.php';
                     <center><h3 class="font">Add a service</h3></center>
                 </div>
                 <div class="panel-body">
-                    <form action="" method="POST" enctype="multipart/form-data">
+                    <form action="verif_inscription.php" method="POST" enctype="multipart/form-data">
                         <div class="row">
                             <div class="col-lg-12 col-xl-126">
                                 <?php
@@ -40,18 +40,36 @@ include 'config.php';
                                 ?>
                                 <div class="form-group">
                                     <!-- Affiche -->
-                                    <label class="font">Select a file(png, jpg, jpeg) : </label>
-                                    <input type="file" name="image" class="form-control input-md" multiple><br>
+                                    <label class="font">Nom : </label>
+                                    <input type="text" name="lastName" placeholder="Nom" class="form-control input-md" multiple><br>
                                     <!-- Service's name -->
-                                    <label class="font">Enter the service's name : </label>
-                                    <input type="text" name="name" placeholder="Service's name" class="form-control input-sm"><br>
+                                    <label class="font">Prénom : </label>
+                                    <input type="text" name="firstName" placeholder="Prénom" class="form-control input-sm"><br>
+                                    <!-- Affiche -->
+                                    <label class="font">Mail : </label>
+                                    <input type="text" name="mail" placeholder="Mail" class="form-control input-md" multiple><br>
+                                    <!-- Affiche -->
+                                    <label class="font">Confirmation mail : </label>
+                                    <input type="text" name="confMail" placeholder="Confirmation mail" class="form-control input-md" multiple><br>
+                                    <!-- Affiche -->
+                                    <label class="font">Mot de passe : </label>
+                                    <input type="text" name="password" placeholder="Mot de passe" class="form-control input-md" multiple><br>
+                                    <!-- Affiche -->
+                                    <label class="font">Confirmation mot de passe : </label>
+                                    <input type="text" name="confPassword" placeholder="Confirmation mot de passe" class="form-control input-md" multiple><br>
+                                    <!-- Affiche -->
+                                    <label class="font">Adresse : </label>
+                                    <input type="text" name="address" placeholder="Adresse" class="form-control input-md" multiple><br>
+                                    <!-- Affiche -->
+                                    <label class="font">Numéro de téléphone : </label>
+                                    <input type="text" name="tel" placeholder="Téléphone" class="form-control input-md" multiple><br>
 
                                 </div>
                             </div>
                         </div>
                         <br><center><input type="submit" name="" value="Validate" class="btn btn-primary"></center><br>
                     </form>
-                    <form action="../../index.php" method="POST">
+                    <form action="../index.php" method="POST">
                         <center><input type="submit" name="" value="Return" class="btn btn-danger"></center>
                     </form>
                 </div>
