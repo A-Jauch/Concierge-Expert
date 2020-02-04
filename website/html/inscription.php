59f321bac45682d6fcb7afd884ce86f0e614878d
buriotVincent <buriot.vincent@gmail.com>
2020-02-04 16:34:58 +0100

    creation verif_inscription page + changment in paths

--- website/html/inscription.php
+++ website/html/inscription.php
@@ -7,8 +7,8 @@ include 'config.php';
 <html>
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Add Service</title>
 </head>
 
@@ -16,7 +16,7 @@ include 'config.php';
 
 <div class="container">
     <div class="">
-        <center><img src="../../img/logo.png" width="200px" ></center>
+        <center><img src="../img/logo.png" width="200px" ></center>
     </div><br>
     <div class="row centered-form">
         <div class="col-lg-12 col-xl-12">
@@ -25,7 +25,7 @@ include 'config.php';
                     <center><h3 class="font">Add a service</h3></center>
                 </div>
                 <div class="panel-body">
-                    <form action="verif_service.php" method="POST" enctype="multipart/form-data">
+                    <form action="" method="POST" enctype="multipart/form-data">
                         <div class="row">
                             <div class="col-lg-12 col-xl-126">
                                 <?php
