5ae02de4c05ec772ff3a4d393e7e0990031f416b
buriotVincent <buriot.vincent@gmail.com>
2020-03-02 10:22:25 +0100

    add all files subscription

--- website/html/subscription.php
+++ website/html/subscription.php
@@ -1,1 +1,91 @@
-<?php
+<?php session_start();
+include 'config.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Abonnement</title>
+</head>
+
+<body class="bodi">
+
+    <header>
+        <nav>
+            <div class="align">
+                <ul>
+                    <li><a href="../index.php">Home</a></li>
+                    <li><a href="#">Services</a></li>
+                    <a href="../index.php" id="logo"><img src="../img/logo.png" width="200px"></a>
+                    <li><a href="subscription.php">Abonnement</a></li>
+
+                    <?php
+                    $connected = isset($_SESSION['mail']) ? true : false;
+                    if($connected) { ?>
+                        <li><a href="deconnection.php"><button type="button" class="btn btn-primary">Déconnexion</button></a></li>
+                    <?php } else { ?>
+                        <li><a href="connection.php"><button type="button" class="btn btn-primary">Client area</button></a></li>
+                    <?php } ?>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row centered-form">
+                <div class="col-lg-12 col-xl-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <center><h3 class="font">Abonnement</h3></center>
+                            <br><br>
+                            <div class="card" style="width: 33%; display: inline-block;">
+                                <div class="card-body">
+                                    <h5 class="card-title" style="text-align: center">Abonnement de base</h5><br>
+                                    <p class="card-text" style="text-align: center">Bénéficiez d'un accès privilégie en ilimité 5j/7 de 9h à 20h</p>
+                                    <br>
+                                    <p class="card-text" style="text-align: center">12h de services/mois</p><br>
+                                    <center><a href="#" class="btn btn-primary">S'abonner</a></center>
+                                </div>
+                            </div>
+                            <div class="card" style="width: 33%; display: inline-block;">
+                                <div class="card-body">
+                                    <h5 class="card-title" style="text-align: center">Abonnement Familial</h5><br>
+                                    <p class="card-text" style="text-align: center">Bénéficiez d'un accès privilégie en ilimité 6j/7 de 9h à 20h</p>
+                                    <br>
+                                    <p class="card-text" style="text-align: center">25h de services/mois</p><br>
+                                    <center><a href="#" class="btn btn-primary">S'abonner</a></center>
+                                </div>
+                            </div>
+                            <div class="card" style="width: 33%; display: inline-block;">
+                                <div class="card-body">
+                                    <h5 class="card-title" style="text-align: center">Abonnement Premium</h5><br>
+                                    <p class="card-text" style="text-align: center">Bénéficiez d'un accès privilégie en ilimité 7j/7 24h/24</p>
+                                    <br>
+                                    <p class="card-text" style="text-align: center">50h de services/mois</p><br>
+                                    <center><a href="#" class="btn btn-primary">S'abonner</a></center>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <br>
+        <section id="bottom">
+            <a href=""><img src="../img/logo.png" width="80"></a>
+            <div><small> Concierge Expert - All rights reserved © </small></div>
+        </section>
+        <br>
+    </footer>
+
+</body>
+
+</html>
+
--- website/index.php
+++ website/index.php
@@ -2,82 +2,102 @@
 include 'html/config.php'; ?>
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <title>Concierge Expert</title>
-  </head>
-  <body>
-    <header>
-        <nav>
-          <div class="align">
-            <ul>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Services</a></li>
-              <a href="index.php" id="logo"><img src="img/logo.png" width="200px"></a>
-              <li><a href="html/subscription.php">Abonnement</a></li>
+</head>
+<body>
+<header>
+    <nav>
+        <div class="align container">
+            <div class="row">
+                <div class="col-lg-12   ">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Services</a></li>
+                        <a href="index.php" id="logo"><img src="img/logo.png" width="200px"></a>
+                        <li><a href="html/subscription.php">Abonnement</a></li>
 
-                <?php
-                $connected = isset($_SESSION['mail']) ? true : false;
-                if($connected) { ?>
-                    <li><a href="html/deconnection.php"><button type="button" class="btn btn-primary">Déconnexion</button></a></li>
-                <?php } else { ?>
-                    <li><a href="html/connection.php"><button type="button" class="btn btn-primary">Client area</button></a></li>
-                <?php } ?>
-            </ul>
-          </div>
-        </nav>
-    </header>
+                        <?php
+                        $connected = isset($_SESSION['mail']) ? true : false;
+                        if ($connected) { ?>
+                            <li><a href="html/deconnection.php">
+                                    <button type="button" class="btn btn-primary">Déconnexion</button>
+                                </a></li>
+                        <?php } else { ?>
+                            <li><a href="html/connection.php">
+                                    <button type="button" class="btn btn-primary">Client area</button>
+                                </a></li>
+                        <?php } ?>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
 
-    <main>
-      <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+<main>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="img/background.jpg" class="d-block w-100" class="background">
-          </div>
+            <div class="carousel-item active">
+                <img src="img/background.jpg" class="d-block w-100" class="background">
+            </div>
         </div>
 
 
-    <center><h1 class="font">Services</h1></center>
+        <center><h1 class="font">Services</h1></center>
 
-    <?php
-          $req2 = $bdd -> prepare("SELECT * FROM SERVICE");
-          $req2 -> execute();
-     ?>
+        <?php
+        $req2 = $bdd->prepare("SELECT * FROM SERVICE");
+        $req2->execute();
+        ?>
 
         <div class="container">
-          <div class="row">
-      <?php if($req2 -> rowCount() > 0): ?>
-      <?php while($row = $req2 -> fetch(PDO::FETCH_ASSOC)): ?>
-         <!-- Ajout d'un service sur l'écran d'accueil -->
+            <div class="row">
+                <?php if ($req2->rowCount() > 0): ?>
+                    <?php while ($row = $req2->fetch(PDO::FETCH_ASSOC)): ?>
+                        <!-- Ajout d'un service sur l'écran d'accueil -->
 
-            <div class="col-lg-4 col-sm-6 col-xs-12">
-              <div class="card text-center box" style="width: 20rem;">
-                <center><img class="size" width="300px" height="300px" src="<?php echo 'html/back_office/' . $row['image']; ?>"></center>
-                <div class="card-body">
-                  <h5 class="card-title"><?php echo '<h3><b>'. $row['name'] . '</b></h3>'; ?></h5>
-                  <a href="#" class="btn btn-primary">Reservation</a>
-                </div>
-              </div>
+                        <div class="col-lg-4 col-sm-6 col-xs-12">
+                            <div class="card text-center box" style="width: 20rem;">
+                                <center><img class="size" width="300px" height="300px"
+                                             src="<?php echo 'html/back_office/' . $row['image']; ?>"></center>
+                                <div class="card-body">
+                                    <h5 class="card-title"><?php echo '<h3><b>' . $row['name'] . '</b></h3>'; ?></h5>
+                                    <a href="#" class="btn btn-primary">Reservation</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    <?php endwhile; ?>
+                <?php endif; ?>
             </div>
-
-      <?php endwhile; ?>
-      <?php endif; ?>
-          </div>
         </div>
 
-    <br><center><a href="html/back_office/add_service.php"><button type="button" class="btn btn-primary">Add a service</button></a></center><br>
-    <center><a href="html/back_office/delete_service.php"><button type="button" class="btn btn-danger">Delete a service</button></a></center></br>
+        <br>
+        <center><a href="html/back_office/add_service.php">
+                <button type="button" class="btn btn-primary">Add a service</button>
+            </a></center>
+        <br>
+        <center><a href="html/back_office/delete_service.php">
+                <button type="button" class="btn btn-danger">Delete a service</button>
+            </a></center>
+        </br>
 
-    </main>
+</main>
 
-  </body>
+<footer>
+    <br>
+    <section id="bottom">
+        <a href=""><img src="img/logo.png" width="80"></a>
+        <div><small> Concierge Expert - All rights reserved © </small></div>
+    </section>
+    <br>
+</footer>
 
-  <footer>
-    <br><section id="bottom">
-      <a href=""><img src="img/logo.png" width="80"></a>
-      <div><small> Concierge Expert - All rights reserved © </small></div>
-    </section> <br>
-  </footer>
+</body>
+
+
 </html>
