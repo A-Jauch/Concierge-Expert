49e4cb10be316bd58a2c9f99ae611257303c4149
Teddy <jfrancoisteddy@gmail.com>
2020-05-04 10:04:51 +0200

    Empty Website

--- website/html/config.php
+++ website/html/config.php
@@ -1,7 +1,7 @@
 <?php
 
   try {
-    $bdd = new PDO('mysql:host=localhost;dbname=concierge_expert','tedanvi','kLKLxEe8M1EfOdvG');
+    $bdd = new PDO('mysql:host=localhost;dbname=concierge_expert;charset=UTF8','tedanvi','kLKLxEe8M1EfOdvG');
   }
   catch(PDOException $error){
     echo ' Problème de connexion à la base de donnée '.$error;
